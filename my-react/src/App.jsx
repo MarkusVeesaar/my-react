f93f48d1ca7f9825f00ad68d7987f5b28b710e0c
Markus <Markus.veesaar@voco.ee>
2026-01-15 14:01:48 +0200

    3. Komponendi sisustamine andmetega

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,10 +1,23 @@
 import Expenseitem from './components/Expensitem'
 
 function App() {
+  const expenses =[ 
+  {
+    date : new Date(2024, 10, 12),
+    title : "new book",
+    price : 30.99
+  },
+  {
+    date : new Date(2024, 10, 12),
+    title : "new book 2",
+    price : 99.99
+  }
+] 
+
   return (
     <div className="App">
-      <Expenseitem/>
-      <Expenseitem/>
+      <Expenseitem data={expenses[0]}/>
+      <Expenseitem data={expenses[1]}/>
     </div>
   )
 }

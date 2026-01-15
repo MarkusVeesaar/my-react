22fc51373f8efe6a4f13d886d71c37a81d435974
Markus <Markus.veesaar@voco.ee>
2026-01-15 15:34:57 +0200

    7. Komponentide kompositsioon ja info edastamine

--- my-react/src/components/Expenses.jsx
+++ my-react/src/components/Expenses.jsx
@@ -1,13 +1,14 @@
 import Expenseitem from './Expensitem.jsx'
+import Card from './Card.jsx'
 import './Expenses.css';
 
 function Expenses(props) {
   
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <Expenseitem data={props.items[0]} />
       <Expenseitem data={props.items[1]} />
-    </div>
+    </Card>
   );
 }
 

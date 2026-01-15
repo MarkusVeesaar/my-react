7b279020f26d1b44724929ed62444ff4e1cc0500
Markus <Markus.veesaar@voco.ee>
2026-01-15 13:35:22 +0200

    2. Uue komponendi loomine ja kujundamine

---
+++ my-react/src/components/Expensitem.jsx
@@ -0,0 +1,15 @@
+import './Expensitem.css'
+
+const Expenseitem = () => {
+    return (
+        <div className='expense-item'>
+            <div>Date</div>
+            <div className='expense-item__description'>
+                <h2>Title</h2>
+                <div className='expense-item__price'>Price</div>
+            </div>
+        </div>
+    )
+}
+
+export default Expenseitem

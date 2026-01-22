4f071cbfd0c91ce438a2a88e46242e4146d65adc
Markus <Markus.veesaar@voco.ee>
2026-01-22 15:36:21 +0200

    11.4 Vormitöötlus Reactis

--- my-react/src/components/NewExpense/ExpenseForm.jsx
+++ my-react/src/components/NewExpense/ExpenseForm.jsx
@@ -21,35 +21,54 @@ const ExpenseForm = (props) => {
             ...userInput,
             enteredPrice: event.target.value
         })
-    } 
+    }
+
         const DateChangeHandler = (event) => {
         setuserInput({
             ...userInput,
             enteredDate: event.target.value
         })
-    } 
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        const expenseData = {
+            title: userInput.enteredTitle,
+            price: userInput.enteredPrice,
+            date: new Date(userInput.enteredDate)
+        } 
+        setuserInput({
+            enteredTitle: '',
+            enteredPrice: '',
+            enteredDate: ''
+        })
+    }
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
           <input 
-          type="text"
-          onChange={titleChangeHandler}/>
+            type="text"
+            onChange={titleChangeHandler}
+            value={userInput.enteredTitle}
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Price</label>
           <input type="number" min="0.01" step="0.01"
             onChange={PriceChangeHandler}
+            value={userInput.enteredPrice}
             />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
           <input type="date" min="2024-11-12" max="2026-01-31"
-            onChange={DateChangeHandler}/>
+            onChange={DateChangeHandler}
+            value={userInput.enteredDate}/>
         </div>
       </div>
 

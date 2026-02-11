466764db222e986595db428491b6e338edaeb8c3
Markus <Markus.veesaar@voco.ee>
2026-02-11 11:40:24 +0200

    16. Ülesanne 6 - valikulise kuvamise täiendamine

--- my-react/src/components/NewExpense/NewExpense.jsx
+++ my-react/src/components/NewExpense/NewExpense.jsx
@@ -1,20 +1,39 @@
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+  const [editForm, setEditForm] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    resetEditFormHandler()
   };
+  
+  function resetEditFormHandler() {
+  setEditForm(false);
+  }
 
-  return (
+  function setEditFormHandler() {
+  setEditForm(true);
+  }
+
+  if (editForm) {
+      return (
     <div className="new-expense">
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
-  );
+    );
+  } else {
+    return (
+      <div className="new-expense">
+        <button onClick={setEditFormHandler}>Add new Expense</button>
+      </div>
+      )
+  }
 };
 
 export default NewExpense;
